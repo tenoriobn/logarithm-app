e6c232abcaaa6b75b1a23286e87ec927069884e3
Bruno Tenorio <tenoriobn@gmail.com>
2026-07-20 11:39:34 -0300

    feat: :sparkles: Adiciona foto Aboro e cria nova herosection

--- src/app/sobre/HeroSection/index.tsx
+++ src/app/sobre/HeroSection/index.tsx
@@ -7,12 +7,11 @@ const HeroSection = () => {
     <section className="bg-surface-950 3xl:px-[1.666vw] 3xl:py-[6.667vw] relative flex min-h-svh flex-col items-center justify-center px-4 py-16 md:px-8 md:py-24">
       <HeroBackground />
 
-      <header className="3xl:gap-[.833vw] z-2 flex flex-col gap-4">
-        <SectionTitle id="hero-section-title">Por trás de toda solução</SectionTitle>
+      <header className="max-lg:gap-4 z-2 flex w-full flex-col items-center @container lg:items-end">
+        <SectionTitle id="hero-section-title" className="uppercase font-aboro leading-[16.32cqw]! w-full text-[16.32cqw]!">logarithm</SectionTitle>
 
-        <SectionDescription id="hero-section-description" className="max-sm:max-w-[288px]">
-          Existe uma forma de pensar, decidir e construir.
-          <br className="hidden sm:max-xl:block" /> É isso que você encontrará nesta página.
+        <SectionDescription id="hero-section-description" className="max-sm:max-w-[288px] w-max">
+          Entenda como transformamos <br className="hidden sm:max-md:block" /> desafios em soluções digitais.
         </SectionDescription>
       </header>
     </section>
