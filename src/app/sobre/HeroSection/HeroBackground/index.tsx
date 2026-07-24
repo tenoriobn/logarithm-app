6c5f3218eb7ba6d810c4624c21446bb9fcd9c78e
Bruno Tenorio <tenoriobn@gmail.com>
2026-07-24 13:29:45 -0300

    refactor: :lipstick: Ajusta estilização do vídeo de fundo da herosection

--- src/app/sobre/HeroSection/HeroBackground/index.tsx
+++ src/app/sobre/HeroSection/HeroBackground/index.tsx
@@ -21,21 +21,20 @@ const HeroBackground = () => {
           (max-width:1440px) 1400px,
           1920px
         "
-        className={`object-cover object-center mix-blend-color-dodge brightness-[0.5] transition-opacity duration-700 ${videoReady ? 'opacity-0' : 'opacity-100'} `}
+        className={`transform-gpu object-cover object-center mix-blend-color-dodge brightness-[0.5] transition-opacity duration-700 will-change-[opacity] ${videoReady ? 'opacity-0' : 'opacity-100'} `}
       />
 
-      {/* <video
+      <video
         autoPlay
         muted
         loop
         playsInline
         preload="auto"
-        onCanPlay={() => setVideoReady(true)}
-        className={`absolute inset-0 h-full w-full object-cover object-[calc(81%+120px)_center] mix-blend-color-dodge brightness-[0.4] transition-opacity duration-700 md:object-[calc(84%+120px)_center] xl:object-center ${videoReady ? 'opacity-100' : 'opacity-0'} `}
+        onPlaying={() => setVideoReady(true)}
+        className={`absolute inset-0 h-full w-full transform-gpu object-cover object-center mix-blend-color-dodge brightness-[0.4] transition-opacity duration-700 will-change-[opacity] ${videoReady ? 'opacity-100' : 'opacity-0'} `}
       >
-        <source src="/videos/about-hero-section.webm" type="video/webm" />
-        <source src="/videos/about-hero-section.mp4" type="video/mp4" />
-      </video> */}
+        <source src="/videos/hero-video.mp4" type="video/mp4" />
+      </video>
 
       <div
         aria-hidden
